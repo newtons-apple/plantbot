7512941925b779597285c5e2400f91ce701a4d8c
newtons-apple <jwc2094@naver.com>
2022-12-15 23:32:42 +0900

    change speed

--- move.py
+++ move.py
@@ -142,9 +142,9 @@ if __name__ == '__main__':
 	try:
 		speed_set = 60
 		setup()
-		move(100,'forward','forward')
+		move(1000,'forward','forward')
 		time.sleep(2)
-		move(100,'backward','backward')
+		move(1000,'backward','backward')
 		time.sleep(2)
 		move(100,'forward','right',1)
 		time.sleep(2)
