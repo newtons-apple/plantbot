de5fd1211bf1306f1dec79d608c5c15b0a809135
newtons-apple <jwc2094@naver.com>
2022-12-16 07:37:56 +0900

    add multiThread

---
+++ multiThread.py
@@ -0,0 +1,32 @@
+from threading import Thread
+from playsound import playsound
+import time
+import move
+def dance():
+    for t in range(0,4,1):
+        print('ab')
+        time.sleep(0.5)
+        print('bc')
+        time.sleep(0.5)
+        move.move(100,'forward','forward')
+        time.sleep(1)
+        move.move(100,'backward','backward')
+        time.sleep(1)
+        move.move(100,'forward','right',1)
+        time.sleep(1)
+        move.move(100,'forward','left',1)
+        time.sleep(1)
+def sing():
+    playsound("christmas_song.wav")
+
+if __name__ == "__main__":
+    START, END = 0, 100000000
+    result = list()
+    th1 = Thread(target=dance, args=(1, START, END//2, result))
+    th2 = Thread(target=sing, args=(2, END//2, END, result))
+    
+    th1.start()
+    th2.start()
+    th1.join()
+    th2.join()
+    move.motorStop()
