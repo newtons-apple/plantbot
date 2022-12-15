b1f50fa20b4b4ac0dac757975d8f116019da483c
newtons-apple <jwc2094@naver.com>
2022-12-16 02:11:48 +0900

    2

--- play.py
+++ play.py
@@ -129,12 +129,12 @@ while True:
         minSize = (int(minW), int(minH)),
        )
     for(x,y,w,h) in faces:
-        # cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
         # Check if confidence is less them 100 ==> "0" is perfect match
         if (confidence < 100):
             id = names[id]
-            # confidence = "  {0}%".format(round(100 - confidence))
+            confidence = "  {0}%".format(round(100 - confidence))
             cv2.imshow('face',happy)
             cv2.waitKey(10)
             playsound("hello.wav")
@@ -150,9 +150,9 @@ while True:
             id = "unknown"
             # confidence = "  {0}%".format(round(100 - confidence))
         
-    #     cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-    #     cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    # cv2.imshow('camera',img) 
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    cv2.imshow('camera',img) 
     if master:
         master=False
         break
